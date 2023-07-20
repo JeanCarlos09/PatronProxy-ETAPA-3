4494ba9ddf6e128f4b727b9347024c3da4401bc5
JeanCarlos09 <136395052+JeanCarlos09@users.noreply.github.com>
2023-07-19 21:41:47 -0500

    ServicioEnvioPaquetes.cs

--- ServicioEnvioPaquetes.cs
+++ ServicioEnvioPaquetes.cs
@@ -6,20 +6,31 @@ public class ServicioEnvioPaquetes : IServicioEnvioPaquetes
 {
     public void EnviarPaquete()
     {
+    // Definición de algunas cadenas para simplificar las opciones de entrada.
       string desea = "si";
       string nodesea = "no";
       string Desea = "1";
       string Nodesea = "2";
+      
+      // Mostrar opciones al usuario.
       Console.WriteLine("Desea enviar paquete:");
       Console.WriteLine("1. si");
       Console.WriteLine("2. no");
+      
+      // Leer la respuesta del usuario.
       string propuesta = Console.ReadLine();
+      
+      // Verificar si el usuario desea enviar el paquete.
       if (propuesta == desea || propuesta == Desea){
+      
+      // Solicitar detalles del destinatario.
       Console.WriteLine("Enviar paquete a : ");
       Console.WriteLine("Ingrese el nombre del destinatorio");
       string nombreDestinatorio = Console.ReadLine();
       Console.WriteLine("Ingrese la dirección del destinatorio");
       string direccionDestinatario = Console.ReadLine();
+
+      // Simular el envío exitoso.
       Console.WriteLine("Envio exitoso ");
         }
     }
